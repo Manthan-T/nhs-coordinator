7b75d119fc3452d574de0a31ff0c938f0b06a8c1
MrFirekiller <91788203+MrFirekiller@users.noreply.github.com>
2022-02-12 09:44:13 +0000

    NHS coordinator start screen and basis of menu

---
+++ core/src/com/github/quintus_cult/nhs_coordinator/main/Devices.java
@@ -0,0 +1,11 @@
+package com.github.quintus_cult.nhs_coordinator.main;
+
+public class Devices {
+
+    String username;
+
+    public void dispose() {
+
+    }
+
+}
--- core/src/com/github/quintus_cult/nhs_coordinator/main/Interface.java
+++ core/src/com/github/quintus_cult/nhs_coordinator/main/Interface.java
@@ -1,49 +1,139 @@
 package com.github.quintus_cult.nhs_coordinator.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
+import java.util.ArrayList;
+
+import javax.management.Notification;
+
 public class Interface extends ApplicationAdapter {
-	static final int WIDTH = 1080;
-	static final int HEIGHT = 1920;
+	static final float WIDTH = 1080;
+	static final float HEIGHT = 1920;
 
 	OrthographicCamera camera;
 	ExtendViewport viewport;
 
-	SpriteBatch batch;
+	SpriteBatch g_logo;
+	float alpha = 0;
+
 	Texture logo;
+	boolean flipLogoAnimation = false;
+	boolean logoDone = false;
+
+	SpriteBatch g_menu;
+	Texture menu_background;
+	Texture add_floor_button;
+	Texture view_floor_button;
+
+	SpriteBatch g_floors;
+	ArrayList<Texture> floors = new ArrayList<Texture>();
+	Texture no_floors;
+
+	SpriteBatch g_devices;
+	Texture current_device;
+	ArrayList<Devices> devices = new ArrayList<Devices>();
 	
 	@Override
-	public void create () {
+	public void create() {
 		camera = new OrthographicCamera();
 		viewport = new ExtendViewport(WIDTH, HEIGHT, camera);
 
-		batch = new SpriteBatch();
+		g_logo = new SpriteBatch();
 		logo = new Texture("logo.png");
+
+		g_devices = new SpriteBatch();
+		current_device = new Texture("current_device.png");
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(0, 0, 1, 1);
-		batch.begin();
-		batch.draw(logo, WIDTH/2 - 343, HEIGHT/2 - 183);
-		batch.end();
+		g_logo.setColor(1, 1, 1, alpha);
+		g_logo.begin();
+		g_logo.draw(logo, WIDTH/2 - 343, HEIGHT/2);
+		g_logo.end();
+		update();
+	}
+
+	public void update() {
+		float delta = Gdx.graphics.getDeltaTime();
+
+		updateLogo(delta);
+
+		if (logoDone) {
+			g_devices.begin();
+			g_devices.draw(current_device, WIDTH/2 - 66, HEIGHT/2 + 66);
+			g_devices.end();
+		}
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
+	public void dispose() {
 		logo.dispose();
+		menu_background.dispose();
+		add_floor_button.dispose();
+		view_floor_button.dispose();
+
+		no_floors.dispose();
+		current_device.dispose();
+
+		for (Texture floor : floors) {
+			floor.dispose();
+		}
+
+		g_logo.dispose();
+		g_menu.dispose();
+		g_floors.dispose();
+		g_devices.dispose();
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		viewport.update(width, height, true);
-		batch.setProjectionMatrix(camera.combined);
+		g_logo.setProjectionMatrix(camera.combined);
+	}
+
+	public void updateLogo(float delta) {
+		if (alpha >= 1) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			flipLogoAnimation = true;
+		}
+
+		if (!flipLogoAnimation) {
+			alpha += delta * 0.4;
+
+		} else {
+			alpha -= delta * 0.4;
+		}
+
+		g_logo.setColor(1, 1, 1, alpha);
+
+		if (alpha < 0) {
+			ScreenUtils.clear(0, 0, 0, 1);
+			logoDone = true;
+
+			g_menu = new SpriteBatch();
+			menu_background = new Texture("menu_background.png");
+			add_floor_button = new Texture("add_floor_button.png");
+			view_floor_button = new Texture("view_floor_button.png");
+
+			g_menu.begin();
+			g_menu.draw(menu_background, 0, HEIGHT + 50);
+			g_menu.draw(add_floor_button, 50, HEIGHT + 50);
+			g_menu.draw(view_floor_button, 700, HEIGHT + 50);
+			g_menu.end();
+		}
+
 	}
 
 }
